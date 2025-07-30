06e5981e7eb43c3d53438294ac3512cb089796fc
Ishaan Shukla <98260156+Ishaan2112@users.noreply.github.com>
2025-07-30 05:53:17 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS cose as newer version 
+
+// alert(2+2) We are using node js not browser 
+
+console.log(3+3);
+console.log("Ishaan");
+
+let name ="Ishaan"
+let age=22
+let isLoggedIn= false
+
+
+//Primitive Datatypes
+/*
+number
+bigint 
+string 
+boolean 
+null
+undefined
+symbol
+*/
+
+console.log(typeof age);
+
