cdb84e546a5bcbeef908883bdc994abfa80141e5
Ishaan Shukla <98260156+Ishaan2112@users.noreply.github.com>
2025-07-30 05:38:52 +0000

    Var and let

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,23 @@
-console.log("Ishaan")
+const accountId = 144553
+let accountEmail = "shuklaishaan01@gmail.com"
+var accountPassword="12345"
+accountCity= "Indore"
+let accountState;
+
+// accountId=2; Not ALlowed 
+
+// Not to use var because of block scope and functional scope.
+accountEmail= "ishaanshukla744@gmail.com"
+accountPassword="211202"
+accountCity="Gurgaon"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState ])
+
+ 
+
+
+
+
+
+
+
