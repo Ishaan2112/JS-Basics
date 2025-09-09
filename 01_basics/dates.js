00f623fc1eaaf21ddaad6cd9d54fb060db823a0b
Ishaan Shukla <98260156+Ishaan2112@users.noreply.github.com>
2025-09-09 18:44:38 +0000

    arraysbasics

---
+++ 01_basics/dates.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleDateString())
+
+let myNewDate = new Date(2023, 0 , 23)
+console.log(myNewDate.toDateString())
+
+
+
+
+
+
+
+
---
+++ 02_basics/arrays.js
@@ -0,0 +1,9 @@
+// const myArr = [0,1,2,3,4,5]
+// console.log(myArr[1])
+
+
+const marvel_heroes = ["thor", "Ironman", "Spiderman"]
+const dc_heroes = ["superman","flash", "batman"]
+
+marvel_heroes.push(dc_heroes)
+console.log(marvel_heroes)
