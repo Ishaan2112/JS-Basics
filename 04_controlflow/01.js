0c63bfd6b2527f653df3735e1fc62704969e3d2d
Ishaan Shukla <98260156+Ishaan2112@users.noreply.github.com>
2025-09-14 12:13:39 +0000

    loops

---
+++ 04_controlflow/01.js
@@ -0,0 +1,21 @@
+const isUserLoggedIn = true
+if(isUserLoggedIn){
+
+}
+
+
+// false : false, 0, -0, BinInt 0n, "", null, undefined, Nan
+
+// true : "0", 'false', ' ',[],{}, function(){}
+
+
+
+//Nullish Coalecing Operator. (??) : null undefined
+
+let val1;
+val1=5 ?? 10
+val2= null ?? 10;
+
+console.log(val1)
+console.log(val2)
+
