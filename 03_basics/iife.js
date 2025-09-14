ec2e1e7c241dcf4871e4f9a82a5d70cd8fc9038b
Ishaan Shukla <98260156+Ishaan2112@users.noreply.github.com>
2025-09-14 10:53:27 +0000

    IIFE concepts

---
+++ 03_basics/iife.js
@@ -0,0 +1,11 @@
+(function chai(){
+    console.log("DB connected")
+})();
+
+(function chai2(){
+    console.log("DB connected")
+})()
+
+
+// Function that calls immediately 
+// Avoids global scope pollutions
